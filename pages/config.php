45a85c6bd7c78af7a7a8453432bbbe7bfde066ad
Jack Mai <mymjack@hotmail.com>
2017-04-17 09:52:35 -0400

    Restore your password

--- pages/config.php
+++ pages/config.php
@@ -2,7 +2,7 @@
 	if (!isset($db)) {
 		define('DB_SERVER', '127.0.0.1');
 		define('DB_USERNAME', 'root');
-		define('DB_PASSWORD', '');
+		define('DB_PASSWORD', 'Shgl123.');
 		define('DB_DATABASE', 'otto_db1');
 		$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die ("Cannot connect to database");
 	}
