3b8baf37089ed43572e1edfcb076273e9afd394c
Jack Mai <mymjack@hotmail.com>
2017-04-17 15:03:21 -0400

    trimming description to 1 line
    Also added after pseudo element with clickable text 'detail'

--- index.php
+++ index.php
@@ -7,196 +7,198 @@
 ?>
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>Otto带物 - 首页</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="assets/css/main.css" />
-	</head>
-	<body class="landing">
+<head>
+	<title>Otto带物 - 首页</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="stylesheet" href="assets/css/otto.css" />
+</head>
+<body class="landing">
 
-		<!-- Header -->
-			<header id="header" class="alt">
-				<h1><strong><a href="index.html">Otto</a></strong> 带物</h1>
-				<nav id="nav">
-					<ul>
-						<li><a href="pages/register.php">登记航班</a></li>
-						<li><a href="pages/search.php?filter=publishdate">航班表</a></li>
-						<li><a href="pages/faq.html">FAQ</a></li>
-						<li><a href="pages/welcome.php">会员登录</a></li>
-					</ul>
-				</nav>
-			</header>
+	<!-- Header -->
+		<header id="header" class="alt">
+			<h1><strong><a href="index.html">Otto</a></strong> 带物</h1>
+			<nav id="nav">
+				<ul>
+					<li><a href="pages/register.php">登记航班</a></li>
+					<li><a href="pages/search.php?filter=publishdate">航班表</a></li>
+					<li><a href="pages/faq.html">FAQ</a></li>
+					<li><a href="pages/welcome.php">会员登录</a></li>
+				</ul>
+			</nav>
+		</header>
 
-			<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+		<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
 
-		<!-- Banner -->
-			<section id="banner">
-				<p>输入您想要带物回国到哪</p>
-				<form method="get" action="pages/search.php?page=1">
-					<div class="index-search">
-						<div class="row 100%">
-							<div class="3u 12u$(xsmall)">
-								<div class="select-wrapper">
-									<select name="dep" id="dep">
-										<option disababled selected value>- 出发地 -</option>
-										<option value="toronto">多伦多 Toronto</option>
-										<option value="markham">万锦 Markham</option>
-										<option value="mississauga">密市 Mississauga</option>
-										<option value="northyork">北约克 North York</option>
-									</select>
-								</div>
+	<!-- Banner -->
+		<section id="banner">
+			<p>输入您想要带物回国到哪</p>
+			<form method="get" action="pages/search.php?page=1">
+				<div class="index-search">
+					<div class="row 100%">
+						<div class="3u 12u$(xsmall)">
+							<div class="select-wrapper">
+								<select name="dep" id="dep">
+									<option disababled selected value>- 出发地 -</option>
+									<option value="toronto">多伦多 Toronto</option>
+									<option value="markham">万锦 Markham</option>
+									<option value="mississauga">密市 Mississauga</option>
+									<option value="northyork">北约克 North York</option>
+								</select>
+							</div>
+						</div>
+						
+						<div class="3u 12u$(xsmall)">
+						<div class="ari-drop">
+							<div class="select-wrapper">
+								<select name="arri" id="arri">
+								<!-- Consider using data base .... Jack -->
+									<option disabled selected value>- 目的地 -</option>
+									<option value="北京">北京</option>
+									<option value="上海">上海</option>
+									<option value="广东">广东</option>
+									<option value="香港">香港</option>
+									<option value="重庆">重庆</option>
+									<option value="天津">天津</option>
+									<option value="澳门">澳门</option>
+									<option disabled>- 拼音排序 -</option>
+									<option value="安徽">安徽</option>
+									<option value="福建">福建</option>
+									<option value="贵州">贵州</option>
+									<option value="河北">河北</option>
+									<option value="黑龙江">黑龙江</option>
+									<option value="河南">河南</option>
+									<option value="湖北">湖北</option>
+									<option value="湖南">湖南</option>
+									<option value="海南">海南</option>
+									<option value="广西">广西</option>
+									<option value="甘肃">甘肃</option>
+									<option value="吉林">吉林</option>
+									<option value="江苏">江苏</option>
+									<option value="江西">江西</option>
+									<option value="辽宁">辽宁</option>
+									<option value="内蒙古">内蒙古</option>
+									<option value="宁夏">宁夏</option>
+									<option value="青海">青海</option>
+									<option value="陕西">陕西</option>
+									<option value="山西">山西</option>
+									<option value="山东">山东</option>
+									<option value="四川">四川</option>
+									<option value="西藏">西藏</option>
+									<option value="新疆">新疆</option>
+									<option value="云南">云南</option>
+									<option value="浙江">浙江</option>
+								</select>
+							</div>
+						</div>
+
+						<input type="hidden" name="page" value="1">
+						</div>
+					</div>
+
+				</div>
+				<br>
+				<div class="index-search-button">
+
+				<ul class="actions">										
+					<li><input type="submit" class="button special small" value="搜索"></li>
+				</ul>
+				</div>
+			</form>
+		</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1">
+				<div class="container 75%">
+					<div class="row 200%">
+						<div class="11u 12u$(medium)">
+							<header class="major">
+								<h2>最新航班：</h2>
+							</header>
+						</div>
+
+						<div class="1u$ 12u$(medium)">
+							<a href="pages/search.php?filter=publishdate">More</a>
+						</div>
+
+						<div class="12u$ 12u$(medium)">
+							<div class="list-group-display-content">
+								<?php 
+								if($result){
+									while($row = mysqli_fetch_array($result)) {
+										$id = $row['id'];
+										$name = $row['name'];
+										$dep = $row['departures'];
+										$date = $row['traveldate'];
+										$arri = $row['arrivals'];
+										$des = $row['description'];
+										echo "<a href='pages/single.php?id_key=$id' class='display-content' style='text-decoration:none;'><h4>$dep -> $arri</h4>$date<br>$name<br>
+										<div class='oneline-desc'>$des</div></a>";
+									}
+								} else {
+									echo "Oops, no data right now. Please come back later.";
+								}
+							?>
 							</div>
 							
-							<div class="3u 12u$(xsmall)">
-							<div class="ari-drop">
-								<div class="select-wrapper">
-									<select name="arri" id="arri">
-									<!-- Consider using data base .... Jack -->
-										<option disabled selected value>- 目的地 -</option>
-										<option value="北京">北京</option>
-										<option value="上海">上海</option>
-										<option value="广东">广东</option>
-										<option value="香港">香港</option>
-										<option value="重庆">重庆</option>
-										<option value="天津">天津</option>
-										<option value="澳门">澳门</option>
-										<option disabled>- 拼音排序 -</option>
-										<option value="安徽">安徽</option>
-										<option value="福建">福建</option>
-										<option value="贵州">贵州</option>
-										<option value="河北">河北</option>
-										<option value="黑龙江">黑龙江</option>
-										<option value="河南">河南</option>
-										<option value="湖北">湖北</option>
-										<option value="湖南">湖南</option>
-										<option value="海南">海南</option>
-										<option value="广西">广西</option>
-										<option value="甘肃">甘肃</option>
-										<option value="吉林">吉林</option>
-										<option value="江苏">江苏</option>
-										<option value="江西">江西</option>
-										<option value="辽宁">辽宁</option>
-										<option value="内蒙古">内蒙古</option>
-										<option value="宁夏">宁夏</option>
-										<option value="青海">青海</option>
-										<option value="陕西">陕西</option>
-										<option value="山西">山西</option>
-										<option value="山东">山东</option>
-										<option value="四川">四川</option>
-										<option value="西藏">西藏</option>
-										<option value="新疆">新疆</option>
-										<option value="云南">云南</option>
-										<option value="浙江">浙江</option>
-									</select>
-								</div>
-							</div>
-
-							<input type="hidden" name="page" value="1">
-							</div>
 						</div>
 
-					</div>
-					<br>
-					<div class="index-search-button">
 
-					<ul class="actions">										
-						<li><input type="submit" class="button special small" value="搜索"></li>
-					</ul>
 					</div>
-				</form>
+				</div>
 			</section>
 
-			<!-- One -->
-				<section id="one" class="wrapper style1">
-					<div class="container 75%">
-						<div class="row 200%">
-							<div class="11u 12u$(medium)">
-								<header class="major">
-									<h2>最新航班：</h2>
-								</header>
-							</div>
-
-							<div class="1u$ 12u$(medium)">
-								<a href="pages/search.php?filter=publishdate">More</a>
-							</div>
-
-							<div class="12u$ 12u$(medium)">
-								<div class="list-group-display-content">
-									<?php 
-									if($result){
-										while($row = mysqli_fetch_array($result)) {
-											$id = $row['id'];
-											$name = $row['name'];
-											$dep = $row['departures'];
-											$date = $row['traveldate'];
-											$arri = $row['arrivals'];
-											$des = $row['description'];
-											echo "<a href='pages/single.php?id_key=$id' class='display-content' style='text-decoration:none;'><h4>$dep -> $arri</h4>$date<br>$name<br>$des</a>";
-										}
-									} else {
-										echo "Oops, no data right now. Please come back later.";
-									}
-								?>
-								</div>
-								
-							</div>
-
-
-						</div>
-					</div>
-				</section>
-
-			<!-- Two -->
-				<section id="two" class="wrapper style2 special">
-					<div class="container">
-						<header class="major">
-							<h2>Otto提醒</h2>
-							<p></p>
-						</header>
-
-						<p>留心每一笔交易</p>
-				</section>
-
-			<!-- Three -->
-				<section id="four" class="wrapper style3 special">
-					<div class="container">
-						<header class="major">
-							<h2>敬请关注!</h2>
-							<!-- <p>Feugiat sed lorem ipsum magna</p> -->
-						</header>
-						<ul class="actions">
-							<li><a id="back-to-top" href="#" class="button special big">返回顶部</a></li>
-						</ul>
-					</div>
-				</section>
-
-		<!-- Footer -->
-			<footer id="footer">
+		<!-- Two -->
+			<section id="two" class="wrapper style2 special">
 				<div class="container">
-					<ul class="copyright">
-						<li>&copy; Otto Group</li>
+					<header class="major">
+						<h2>Otto提醒</h2>
+						<p></p>
+					</header>
+
+					<p>留心每一笔交易</p>
+			</section>
+
+		<!-- Three -->
+			<section id="four" class="wrapper style3 special">
+				<div class="container">
+					<header class="major">
+						<h2>敬请关注!</h2>
+						<!-- <p>Feugiat sed lorem ipsum magna</p> -->
+					</header>
+					<ul class="actions">
+						<li><a id="back-to-top" href="#" class="button special big">返回顶部</a></li>
 					</ul>
 				</div>
-			</footer>
+			</section>
 
-		<!-- Scripts -->
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-			<script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
-			<script>
-			  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	<!-- Footer -->
+		<footer id="footer">
+			<div class="container">
+				<ul class="copyright">
+					<li>&copy; Otto Group</li>
+				</ul>
+			</div>
+		</footer>
 
-			  ga('create', 'UA-97380931-1', 'auto');
-			  ga('send', 'pageview');
+	<!-- Scripts -->
+		<script src="assets/js/jquery.min.js"></script>
+		<script src="assets/js/skel.min.js"></script>
+		<script src="assets/js/util.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-			</script>
+		  ga('create', 'UA-97380931-1', 'auto');
+		  ga('send', 'pageview');
+
+		</script>
 
 
-	</body>
+</body>
 </html>
--- pages/search.php
+++ pages/search.php
@@ -37,222 +37,224 @@
 ?>
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>航班表 - Otto带物</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="../assets/css/main.css" />
-	</head>
-	<body>
-		<!-- Header -->
-		<header id="header">
-			<h1><strong><a href="../index.php">Otto首页</a></strong> 航班表</h1>
-			<nav id="nav">
-				<ul>
-					<li><a href="../index.php">首页</a></li>
-					<li><a href="register.php">登记航班</a></li>
-					<li><a href="search.php?filter=publishdate">航班表</a></li>
-					<li><a href="faq.html">FAQ</a></li>
-					<li><a href="welcome.php">会员登录</a></li>
-				</ul>
-			</nav>
-		</header>
+<head>
+	<title>航班表 - Otto带物</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<link rel="stylesheet" href="../assets/css/main.css" />
+	<link rel="stylesheet" href="../assets/css/otto.css" />
+</head>
+<body>
+	<!-- Header -->
+	<header id="header">
+		<h1><strong><a href="../index.php">Otto首页</a></strong> 航班表</h1>
+		<nav id="nav">
+			<ul>
+				<li><a href="../index.php">首页</a></li>
+				<li><a href="register.php">登记航班</a></li>
+				<li><a href="search.php?filter=publishdate">航班表</a></li>
+				<li><a href="faq.html">FAQ</a></li>
+				<li><a href="welcome.php">会员登录</a></li>
+			</ul>
+		</nav>
+	</header>
 
-		<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
-		<!-- Main -->
-		<section id="main" class="wrapper">
-			<div class="container">
-				
-				<header class="major special">
-					<h2>搜索结果：</h2>
-					<!--<p>Returns Search Results</p>-->
-				</header>
-				<p></p>
-				<div class="row 150%">
-					<div class="6u 12u$(xsmall)">
-						<form class="flight-filter" method="get"> <!-- Implement keep input after submit -->
+	<a href="#navPanel" class="navPanelToggle"><span class="fa fa-bars"></span></a>
+	<!-- Main -->
+	<section id="main" class="wrapper">
+		<div class="container">
+			
+			<header class="major special">
+				<h2>搜索结果：</h2>
+				<!--<p>Returns Search Results</p>-->
+			</header>
+			<p></p>
+			<div class="row 150%">
+				<div class="6u 12u$(xsmall)">
+					<form class="flight-filter" method="get"> <!-- Implement keep input after submit -->
 
-							<div class="row uniform 50%">
-								<div class="5u 12u$(xsmall)">
-									<div class="select-wrapper">
-									<select name="dep" id="dep">
-										<option disababled selected value>- 出发地 -</option>
-										<option value="toronto">多伦多 Toronto</option>
-										<option value="markham">万锦 Markham</option>
-										<option value="mississauga">密市 Mississauga</option>
-										<option value="northyork">北约克 North York</option>
-									</select>
-									</div>
+						<div class="row uniform 50%">
+							<div class="5u 12u$(xsmall)">
+								<div class="select-wrapper">
+								<select name="dep" id="dep">
+									<option disababled selected value>- 出发地 -</option>
+									<option value="toronto">多伦多 Toronto</option>
+									<option value="markham">万锦 Markham</option>
+									<option value="mississauga">密市 Mississauga</option>
+									<option value="northyork">北约克 North York</option>
+								</select>
 								</div>
-
-								<div class="2u 12u$(xsmall">
-									<p>-></p>
-								</div>
-
-								<div class="5u$ 12u$(xsmall)">
-									<div class="select-wrapper">
-									<select name="arri" id="arri">
-										<option disabled selected value>- 目的地 -</option>
-										<option value="北京">北京</option>
-										<option value="上海">上海</option>
-										<option value="广东">广东</option>
-										<option value="香港">香港</option>
-										<option value="重庆">重庆</option>
-										<option value="天津">天津</option>
-										<option value="澳门">澳门</option>
-										<option disabled>- 拼音排序 -</option>
-										<option value="安徽">安徽</option>
-										<option value="福建">福建</option>
-										<option value="贵州">贵州</option>
-										<option value="河北">河北</option>
-										<option value="黑龙江">黑龙江</option>
-										<option value="河南">河南</option>
-										<option value="湖北">湖北</option>
-										<option value="湖南">湖南</option>
-										<option value="海南">海南</option>
-										<option value="广西">广西</option>
-										<option value="甘肃">甘肃</option>
-										<option value="吉林">吉林</option>
-										<option value="江苏">江苏</option>
-										<option value="江西">江西</option>
-										<option value="辽宁">辽宁</option>
-										<option value="内蒙古">内蒙古</option>
-										<option value="宁夏">宁夏</option>
-										<option value="青海">青海</option>
-										<option value="陕西">陕西</option>
-										<option value="山西">山西</option>
-										<option value="山东">山东</option>
-										<option value="四川">四川</option>
-										<option value="西藏">西藏</option>
-										<option value="新疆">新疆</option>
-										<option value="云南">云南</option>
-										<option value="浙江">浙江</option>
-									</select>
-								</div>
-								</div>
-										
-								<div class="6u 12u$(xsmall)">
-									<div class="select-wrapper">
-										<select name="filter">
-											<option disabled selected value>- 分类 -</option>
-											<option value="traveldate">出发日期</option>
-											<option value="publishdate">发表日期</option>
-											<!--<option value="distance">Distance</option>-->
-										</select>
-									</div>
-								</div>
-								<input type="hidden" name="page" value="1">
-
-								<!--<input type="submit" class="button special" value="submit">-->
-										
-								
-									
-										<div class="12u$">
-											<ul class="actions">
-												<li><input class="button special small" value="更新" type="submit"></li>
-											</ul>
-										</div>
-									
-								
 							</div>
-						</form>
 
-						<div class="12u$ 12u$(medium)">
-							<div class="main-group-display-content">
-								<?php 
-									//if($result){
-									while($row = mysqli_fetch_array($result)) {
-										$id = $row['id'];
-										$name = $row['name'];
-										$dep = $row['departures'];
-										$date = $row['traveldate'];
-										$arri = $row['arrivals'];
-										$des = $row['description'];
-										//echo "<a href='single.php?id_key=$id' class='display-content' style='text-decoration:none;'><br>$name<br>$dep -> $arri<br>$des</a>";
-										echo "<a href='single.php?id_key=$id' class='display-content' style='text-decoration:none;'><h4>$dep -> $arri</h4>$date<br>$name<br>$des</a>";
-										}
-									//}
-								?>
+							<div class="2u 12u$(xsmall">
+								<p>-></p>
 							</div>
+
+							<div class="5u$ 12u$(xsmall)">
+								<div class="select-wrapper">
+								<select name="arri" id="arri">
+									<option disabled selected value>- 目的地 -</option>
+									<option value="北京">北京</option>
+									<option value="上海">上海</option>
+									<option value="广东">广东</option>
+									<option value="香港">香港</option>
+									<option value="重庆">重庆</option>
+									<option value="天津">天津</option>
+									<option value="澳门">澳门</option>
+									<option disabled>- 拼音排序 -</option>
+									<option value="安徽">安徽</option>
+									<option value="福建">福建</option>
+									<option value="贵州">贵州</option>
+									<option value="河北">河北</option>
+									<option value="黑龙江">黑龙江</option>
+									<option value="河南">河南</option>
+									<option value="湖北">湖北</option>
+									<option value="湖南">湖南</option>
+									<option value="海南">海南</option>
+									<option value="广西">广西</option>
+									<option value="甘肃">甘肃</option>
+									<option value="吉林">吉林</option>
+									<option value="江苏">江苏</option>
+									<option value="江西">江西</option>
+									<option value="辽宁">辽宁</option>
+									<option value="内蒙古">内蒙古</option>
+									<option value="宁夏">宁夏</option>
+									<option value="青海">青海</option>
+									<option value="陕西">陕西</option>
+									<option value="山西">山西</option>
+									<option value="山东">山东</option>
+									<option value="四川">四川</option>
+									<option value="西藏">西藏</option>
+									<option value="新疆">新疆</option>
+									<option value="云南">云南</option>
+									<option value="浙江">浙江</option>
+								</select>
+							</div>
+							</div>
+									
+							<div class="6u 12u$(xsmall)">
+								<div class="select-wrapper">
+									<select name="filter">
+										<option disabled selected value>- 分类 -</option>
+										<option value="traveldate">出发日期</option>
+										<option value="publishdate">发表日期</option>
+										<!--<option value="distance">Distance</option>-->
+									</select>
+								</div>
+							</div>
+							<input type="hidden" name="page" value="1">
+
+							<!--<input type="submit" class="button special" value="submit">-->
+									
+							
+								
+									<div class="12u$">
+										<ul class="actions">
+											<li><input class="button special small" value="更新" type="submit"></li>
+										</ul>
+									</div>
+								
+							
 						</div>
+					</form>
 
-								<?php 
-									// Handling pages
-									function str_replace_first($from, $to, $subject){
-									    $from = '/'.preg_quote($from, '/').'/';
-
-									    return preg_replace($from, $to, $subject, 1);
+					<div class="12u$ 12u$(medium)">
+						<div class="main-group-display-content">
+							<?php 
+								//if($result){
+								while($row = mysqli_fetch_array($result)) {
+									$id = $row['id'];
+									$name = $row['name'];
+									$dep = $row['departures'];
+									$date = $row['traveldate'];
+									$arri = $row['arrivals'];
+									$des = $row['description'];
+									//echo "<a href='single.php?id_key=$id' class='display-content' style='text-decoration:none;'><br>$name<br>$dep -> $arri<br>$des</a>";
+									echo "<a href='single.php?id_key=$id' class='display-content' style='text-decoration:none;'><h4>$dep -> $arri</h4>$date<br>$name<br>
+									<div class='oneline-desc'>$des</div></a>";
 									}
-									$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-									$new_link = str_replace_first('&page=' . $page,'', $actual_link);
-									if($page>1)
-									{
-										echo "<a href='$new_link&page=". ($page-1)."' class='button'>PREVIOUS</a>";
-									}
-									// If page is 1, change page to 2
-									if($page!=$total) {
-										echo "<a href='$new_link&page=". ($page+1)."' class='button'>NEXT</a>";
-									}
-									echo "<ul class='page'>";
-									for($i=1;$i<=$total;$i++)
-									{
-									if($i==$page) { echo "<li class='current'>".$i."</li>"; }
-
-									//else { echo "<li><a href='?page=".$i."'>".$i."</a></li>"; }
-									else { echo "<li><a href='$new_link&page=".$i."'>".$i."</a></li>"; }
-									}
-									echo "</ul>";
-								?>
-
+								//}
+							?>
+						</div>
 					</div>
 
-				<!-- Right Col -->	
-				<div class="6u$ 12u$(xsmall)">
-					<!-- Map -->
-					<div id="map" style="width:100%;height:800px;"></div>
-						<script>
-							function myMap() {
-							  var myCenter = new google.maps.LatLng(43.6532, -79.3832);
-							  var mapCanvas = document.getElementById("map");
-							  var mapOptions = {center: myCenter, zoom: 10};
-							  var map = new google.maps.Map(mapCanvas, mapOptions);
-							  var marker = new google.maps.Marker({position:myCenter});
-							  marker.setMap(map);
+							<?php 
+								// Handling pages
+								function str_replace_first($from, $to, $subject){
+								    $from = '/'.preg_quote($from, '/').'/';
 
-							  // New marker
-							  var newPoint = new google.maps.LatLng(43.75,-79.21);
-							  var marker2 = new google.maps.Marker({position:newPoint, 
-							  	icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=12|FF0000|000000',});
-							  marker2.setMap(map);
-							}
+								    return preg_replace($from, $to, $subject, 1);
+								}
+								$actual_link = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+								$new_link = str_replace_first('&page=' . $page,'', $actual_link);
+								if($page>1)
+								{
+									echo "<a href='$new_link&page=". ($page-1)."' class='button'>PREVIOUS</a>";
+								}
+								// If page is 1, change page to 2
+								if($page!=$total) {
+									echo "<a href='$new_link&page=". ($page+1)."' class='button'>NEXT</a>";
+								}
+								echo "<ul class='page'>";
+								for($i=1;$i<=$total;$i++)
+								{
+								if($i==$page) { echo "<li class='current'>".$i."</li>"; }
 
-							var myLatlng = new google.maps.LatLng(44.75,-79.21);  
-							var marker3 = new google.maps.Marker({  
-						        position: myLatlng,   
-						        map: map,  
-						        icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=123123|FF0000|000000'  
-						    })
-						    marker3.setMap(map);
-						</script>
+								//else { echo "<li><a href='?page=".$i."'>".$i."</a></li>"; }
+								else { echo "<li><a href='$new_link&page=".$i."'>".$i."</a></li>"; }
+								}
+								echo "</ul>";
+							?>
 
-						<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDWSdCxe0Mlwiw7cvZielK6kvlt_naC9E&callback=myMap"></script>
 				</div>
-			</section>
 
-		<!-- Footer -->
-		<footer id="footer">
-			<div class="container">
-				<ul class="copyright">
-					<li>&copy; Otto Group</li>
-				</ul>
+			<!-- Right Col -->	
+			<div class="6u$ 12u$(xsmall)">
+				<!-- Map -->
+				<div id="map" style="width:100%;height:800px;"></div>
+					<script>
+						function myMap() {
+						  var myCenter = new google.maps.LatLng(43.6532, -79.3832);
+						  var mapCanvas = document.getElementById("map");
+						  var mapOptions = {center: myCenter, zoom: 10};
+						  var map = new google.maps.Map(mapCanvas, mapOptions);
+						  var marker = new google.maps.Marker({position:myCenter});
+						  marker.setMap(map);
+
+						  // New marker
+						  var newPoint = new google.maps.LatLng(43.75,-79.21);
+						  var marker2 = new google.maps.Marker({position:newPoint, 
+						  	icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=12|FF0000|000000',});
+						  marker2.setMap(map);
+						}
+
+						var myLatlng = new google.maps.LatLng(44.75,-79.21);  
+						var marker3 = new google.maps.Marker({  
+					        position: myLatlng,   
+					        map: map,  
+					        icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=123123|FF0000|000000'  
+					    })
+					    marker3.setMap(map);
+					</script>
+
+					<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDWSdCxe0Mlwiw7cvZielK6kvlt_naC9E&callback=myMap"></script>
 			</div>
-		</footer>
+		</section>
 
-		<!-- Scripts -->
-		<script src="../assets/js/jquery.min.js"></script>
-		<script src="../assets/js/skel.min.js"></script>
-		<script src="../assets/js/util.js"></script>
-		<script src="../assets/js/main.js"></script>
-		<script src="../bootstrap-3.3.7-dist/js/bootstrap.js"></script>
-	</body>
+	<!-- Footer -->
+	<footer id="footer">
+		<div class="container">
+			<ul class="copyright">
+				<li>&copy; Otto Group</li>
+			</ul>
+		</div>
+	</footer>
+
+	<!-- Scripts -->
+	<script src="../assets/js/jquery.min.js"></script>
+	<script src="../assets/js/skel.min.js"></script>
+	<script src="../assets/js/util.js"></script>
+	<script src="../assets/js/main.js"></script>
+	<script src="../bootstrap-3.3.7-dist/js/bootstrap.js"></script>
+</body>
 </html>
