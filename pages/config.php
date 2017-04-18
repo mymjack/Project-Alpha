a85bbe38e381efea1d90fa7cbdd7782651597542
Jack Mai <mymjack@hotmail.com>
2017-04-18 04:29:37 -0400

    adding auto remove outdated flight register entries.
    Initially commented out. Please Back Up your db before uncommenting out
    this line at middle of config.php

--- pages/config.php
+++ pages/config.php
@@ -3,9 +3,13 @@
 		define('DB_SERVER', '127.0.0.1');
 		define('DB_USERNAME', 'root');
 		define('DB_PASSWORD', 'Shgl123.');
+		// define('DB_PASSWORD', '');
 		define('DB_DATABASE', 'otto_db1');
 		$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die ("Cannot connect to database");
 	}
+
+	// Dangerous! This line removes registers that are out of date
+	// include("rm-register.php");
 
 	// Takes a list of variables. Return true iff any is not set/empty
 	function hasEmpty($lst) {
