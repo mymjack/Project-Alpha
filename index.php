47cf4e269d69905135964164382b976a67e07e7e
Jack Mai <mymjack@hotmail.com>
2017-04-18 01:03:33 -0400

    Re arranged index page
    Massive changes and fixes to styles

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 	<title>Otto带物 - 首页</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="stylesheet" href="assets/css/main.css" />
+	<link rel="stylesheet" href="assets/css/main-cleaned.css" />
 	<link rel="stylesheet" href="assets/css/otto.css" />
 
 	<!-- Select2 datalist files -->
@@ -47,94 +47,76 @@
 			<p>输入您想要带物回国到哪</p>
 			<form method="get" action="pages/search.php?page=1">
 				<div class="index-search">
-					<div class="row 100%">
-						<div class="3u 12u$(xsmall)">
-							<div class="select-wrapper">
-								<select name="dep" id="dep">
-									<option disababled selected value></option>
-									<option value="toronto">多伦多 Toronto</option>
-									<option value="markham">万锦 Markham</option>
-									<option value="mississauga">密市 Mississauga</option>
-									<option value="northyork">北约克 North York</option>
-								</select>
-							</div>
-						</div>
-						
-						<div class="3u 12u$(xsmall)">
-						<div class="ari-drop">
-							<div class="select-wrapper">
-								<select name="arri" id="arri">
-								<!-- Consider using data base .... Jack -->
-									<option disabled selected value></option>
-									<option value="北京">北京</option>
-									<option value="上海">上海</option>
-									<option value="广东">广东</option>
-									<option value="香港">香港</option>
-									<option value="重庆">重庆</option>
-									<option value="天津">天津</option>
-									<option value="澳门">澳门</option>
-									<option disabled>- 拼音排序 -</option>
-									<option value="安徽">安徽</option>
-									<option value="福建">福建</option>
-									<option value="贵州">贵州</option>
-									<option value="河北">河北</option>
-									<option value="黑龙江">黑龙江</option>
-									<option value="河南">河南</option>
-									<option value="湖北">湖北</option>
-									<option value="湖南">湖南</option>
-									<option value="海南">海南</option>
-									<option value="广西">广西</option>
-									<option value="甘肃">甘肃</option>
-									<option value="吉林">吉林</option>
-									<option value="江苏">江苏</option>
-									<option value="江西">江西</option>
-									<option value="辽宁">辽宁</option>
-									<option value="内蒙古">内蒙古</option>
-									<option value="宁夏">宁夏</option>
-									<option value="青海">青海</option>
-									<option value="陕西">陕西</option>
-									<option value="山西">山西</option>
-									<option value="山东">山东</option>
-									<option value="四川">四川</option>
-									<option value="西藏">西藏</option>
-									<option value="新疆">新疆</option>
-									<option value="云南">云南</option>
-									<option value="浙江">浙江</option>
-								</select>
-							</div>
-						</div>
-
-						<input type="hidden" name="page" value="1">
-						</div>
+					<div class="col-xs-12 col-sm-7 col-md-3">
+						<select name="dep" id="dep">
+							<option disababled selected value></option>
+							<option value="toronto">多伦多 Toronto</option>
+							<option value="markham">万锦 Markham</option>
+							<option value="mississauga">密市 Mississauga</option>
+							<option value="northyork">北约克 North York</option>
+						</select>
 					</div>
-
+					<div class="col-xs-12 col-sm-7 col-md-3">
+						<select name="arri" id="arri">
+						<!-- Consider using data base .... Jack -->
+							<option disabled selected value></option>
+							<option value="北京">北京</option>
+							<option value="上海">上海</option>
+							<option value="广东">广东</option>
+							<option value="香港">香港</option>
+							<option value="重庆">重庆</option>
+							<option value="天津">天津</option>
+							<option value="澳门">澳门</option>
+							<option disabled>- 拼音排序 -</option>
+							<option value="安徽">安徽</option>
+							<option value="福建">福建</option>
+							<option value="贵州">贵州</option>
+							<option value="河北">河北</option>
+							<option value="黑龙江">黑龙江</option>
+							<option value="河南">河南</option>
+							<option value="湖北">湖北</option>
+							<option value="湖南">湖南</option>
+							<option value="海南">海南</option>
+							<option value="广西">广西</option>
+							<option value="甘肃">甘肃</option>
+							<option value="吉林">吉林</option>
+							<option value="江苏">江苏</option>
+							<option value="江西">江西</option>
+							<option value="辽宁">辽宁</option>
+							<option value="内蒙古">内蒙古</option>
+							<option value="宁夏">宁夏</option>
+							<option value="青海">青海</option>
+							<option value="陕西">陕西</option>
+							<option value="山西">山西</option>
+							<option value="山东">山东</option>
+							<option value="四川">四川</option>
+							<option value="西藏">西藏</option>
+							<option value="新疆">新疆</option>
+							<option value="云南">云南</option>
+							<option value="浙江">浙江</option>
+						</select>
+					</div>
 				</div>
 				<br>
-				<div class="index-search-button">
-
-				<ul class="actions">										
-					<li><input type="submit" class="button special small" value="搜索"></li>
-				</ul>
-				</div>
+				<input type="submit" class="button special small wide" value="搜索">
 			</form>
 		</section>
 
 		<!-- One -->
 			<section id="one" class="wrapper style1">
-				<div class="container 75%">
-					<div class="row 200%">
-						<div class="11u 12u$(medium)">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-9">
 							<header class="major">
 								<h2>最新航班：</h2>
 							</header>
 						</div>
 
-						<div class="1u$ 12u$(medium)">
-							<a href="pages/search.php?filter=publishdate">More</a>
+						<div class="col-xs-3">
+							<a class="badge" href="pages/search.php?filter=publishdate"><button>More</button></a>
 						</div>
 
-						<div class="12u$ 12u$(medium)">
-							<div class="list-group-display-content">
+							<div class="list-group-display-content col-xs-12">
 								<?php 
 								if($result){
 									while($row = mysqli_fetch_array($result)) {
@@ -144,14 +126,19 @@
 										$date = $row['traveldate'];
 										$arri = $row['arrivals'];
 										$des = $row['description'];
-										echo "<a href='pages/single.php?id_key=$id' class='display-content' style='text-decoration:none;'><h4>$dep -> $arri</h4>$date<br>$name<br>
-										<div class='oneline-desc'>$des</div></a>";
+										echo "<a href='pages/single.php?id_key=$id' class='display-content' style='text-decoration:none;'>
+											<div class='name-date'>
+												<div class='col-xs-12 col-sm-7'>
+													<strong>$name</strong> - $date
+												</div>
+												<h4 class='col-xs-12 col-sm-5'>$dep -> $arri</h4>
+											</div>
+											<div class='oneline-desc'>$des</div></a>";
 									}
 								} else {
 									echo "Oops, no data right now. Please come back later.";
 								}
 							?>
-							</div>
 							
 						</div>
 
@@ -209,7 +196,7 @@
 		  ga('send', 'pageview');
 
 		</script>
-		<script src="select2/js/select2.min.js"></script>
+		<script src="select2/js/select2.js"></script>
 		<script type="text/javascript">
 			$('#dep').select2({
 				placeholder: "- 出发地 -",
